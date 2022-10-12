14b4ec9911f620015886938c38b7a92a5d3b7d99
Albert Yuan <aly11@duke.edu>
2022-10-12 17:14:43 -0400

    patched product_review table

--- db/sqlCreate/4createProductReviewTable.sql
+++ db/sqlCreate/4createProductReviewTable.sql
@@ -1,7 +1,8 @@
 -- Product Feedback (Aaric)
-CREATE TABLE ProductReviews (
+drop table if exists ProductReviews;
+CREATE TABLE if not exists ProductReviews (
     user_id INT NOT NULL REFERENCES Users (user_id),
-	product_id INT NOT NULL REFERENCES Purchases (product_id),
+	product_id INT NOT NULL REFERENCES Products (product_id),
 	rating INT NOT NULL CONSTRAINT rating_limit CHECK (rating BETWEEN 1 AND 5),
 	review VARCHAR NOT NULL
 );
