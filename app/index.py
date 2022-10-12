b3019dd854c6b800561e1376d5c2fade707c9150
Albert Yuan <aly11@duke.edu>
2022-10-12 17:32:39 -0400

    patched main to work w newly defined USER and PRODUCT tables

--- app/index.py
+++ app/index.py
@@ -3,7 +3,7 @@ from flask_login import current_user
 import datetime
 
 from .models.product import Product
-from .models.purchase import Purchase
+from .models.order import Order
 
 from flask import Blueprint
 bp = Blueprint('index', __name__)
@@ -15,11 +15,11 @@ def index():
     products = Product.get_all(True)
     # find the products current user has bought:
     if current_user.is_authenticated:
-        purchases = Purchase.get_all_by_uid_since(
+        orders = Order.get_all_by_uid_since(
             current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
     else:
-        purchases = None
+        orders = None
     # render the page by adding information to the index.html file
     return render_template('index.html',
                            avail_products=products,
-                           purchase_history=purchases)
+                           purchase_history=orders)
