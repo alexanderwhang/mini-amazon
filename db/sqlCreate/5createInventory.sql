61be2f3a541c4a5ab5d8b87fe5a2a5d4f17c18bf
Parker Vakili <prv5@duke.edu>
2022-10-12 23:26:07 -0400

    added everything before init functions

---
+++ db/sqlCreate/5createInventory.sql
@@ -0,0 +1,7 @@
+drop table if exists Inventory cascade;
+CREATE TABLE if not exists Inventory (
+    user_id INT REFERENCES Users(user_id),
+    product_id INT REFERENCES Products(product_id),
+    quantity INT NOT NULL,
+    price DECIMAL(12,2) NOT NULL REFERENCES Products(price)
+);
