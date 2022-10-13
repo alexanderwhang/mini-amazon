6353d25a33297d0ca2a11464d35b14e4fdc50486
Advaita Singh <as992@duke.edu>
2022-10-12 22:38:01 -0400

    Fixed the product.py

--- app/models/product.py
+++ app/models/product.py
@@ -2,11 +2,17 @@ from flask import current_app as app
 
 
 class Product:
-    def __init__(self, id, name, price, available):
-        self.id = id
+    def __init__(self, product_id, user_id, category, name, description, price, imageurl, quantity, available, avg_rating):
+        self.product_id = product_id
+        self.user_id = user_id
+        self.category = category
         self.name = name
+        self.description = description
         self.price = price
+        self.imageurl = imageurl
+        self.quantity = quantity
         self.available = available
+        self.avg_rating = avg_rating
 
     @staticmethod
     def get(id):
