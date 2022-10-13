b677c144d07eed02cdbcf17312ff8a027fae3365
Advaita Singh <as992@duke.edu>
2022-10-12 22:33:00 -0400

    Fixed the products loading.

--- db/sqlLoad/1loadProductTables.sql
+++ db/sqlLoad/1loadProductTables.sql
@@ -1,4 +1,3 @@
+\COPY Categories FROM 'Categories.csv' WITH DELIMITER ',' NULL '' CSV
+
 \COPY Products FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.products_id_seq',
-                         (SELECT MAX(product_id)+1 FROM Products),
-                         false);
