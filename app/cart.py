e1113fba7ed6650e60e74dd68cab55a85de0a038
Alex Whang <amw166@duke.edu>
2022-11-08 01:14:32 -0500

    updated cart db schema

--- app/cart.py
+++ app/cart.py
@@ -1,11 +1,8 @@
 from flask import current_app as app
 from flask import render_template, redirect, url_for, flash, request
 from werkzeug.urls import url_parse
-from flask_login import login_user, logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
-
+from flask_login import current_user
 from .models.user import User, BadUpdateException
 from flask import Blueprint
 bp = Blueprint('cart', __name__)
@@ -24,12 +21,13 @@ class Cart:
         self.quantity = quantity
         self.time_added_to_cart = time_added_to_cart
 
+# get and get_all_by_uid_since queries outdated
 
     @staticmethod
     def get(id):
         rows = app.db.execute('''
 SELECT id, uid, pid, quantity, time_added_to_cart
-FROM Carts
+FROM Cart
 WHERE id = :id
 ''',
                               id=id)
@@ -39,7 +37,7 @@ WHERE id = :id
     def get_all_by_uid_since(uid, since):
         rows = app.db.execute('''
 SELECT id, uid, pid, quantity, time_added_to_cart
-FROM Carts
+FROM Cart
 WHERE uid = :uid
 AND time_added_to_cart >= :since
 ORDER BY time_added_to_cart DESC
@@ -51,10 +49,10 @@ ORDER BY time_added_to_cart DESC
     @staticmethod
     def get_all_by_uid(uid):
         rows = app.db.execute('''
-SELECT Carts.id, uid, pid, name, price, quantity, time_added_to_cart
-FROM Carts, Products
+SELECT Cart.user_id, Cart.product_id, Products.name, Products.price, quantity, time_added_to_cart
+FROM Cart, Products
 WHERE uid = :uid
-AND Carts.pid = Products.id
+AND Cart.pid = Products.id
 ORDER BY time_added_to_cart DESC
 ''',
                               uid=uid)
@@ -68,7 +66,7 @@ class CartPrice:
     def getPrice(uid):
         price = app.db.execute('''
 SELECT SUM(price * quantity)
-FROM Carts, Products
+FROM Cart, Products
 WHERE uid = :uid
 AND Carts.pid = Products.id
 ''',
@@ -77,5 +75,6 @@ AND Carts.pid = Products.id
 
 @bp.route('/cart')
 def cart():
-    #user = User.get(current_user.id)
-    return render_template('cart.html', title='Cart')
+    user = User.get(current_user.id)
+    cart = Cart.get_all_by_uid(current_user.id)
+    return render_template('cart.html', title='Cart', user=user)
