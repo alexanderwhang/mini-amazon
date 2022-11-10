3a6345af167fe3c8e9a2ad45c02146059eefc02f
Albert Yuan <aly11@duke.edu>
2022-11-10 15:38:30 -0500

    updated schema and gen.py

--- db/sqlCreate/6createFulfillment.sql
+++
@@ -1,6 +0,0 @@
-drop table if exists Fulfillment cascade;
-CREATE TABLE if not exists Fulfillment (
-    user_id INT REFERENCES Users(user_id),
-    order_id INT REFERENCES Orders(order_id),
-    fulfillment_status VARCHAR not null check(fulfillment_status in ('ordered', 'shipped', 'delivered')) 
-);
