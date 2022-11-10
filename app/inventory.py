516a4cc759269e5219cad63a5f77fa53b5249397
Parker Vakili <prv5@duke.edu>
2022-11-10 18:36:23 -0500

    added buttons

--- app/inventory.py
+++ app/inventory.py
@@ -19,7 +19,8 @@ bp = Blueprint('inventory', __name__)
 def seller():
     # get all available products for sale:
     inventory = Inventory.get_all_inventories_by_user(current_user.id)
-    fulfillment = Fulfillment.get_all_fulfillment_by_user(current_user.id)
+    #delete = Inventory.remove_product(current_user.id)
+    fulfillment = Fulfillment.get_all_fulfillment_by_user(current_user.id, )
     # find the products current user has bought:
     #if current_user.is_authenticated:
     #    orders = Purchase.get_all_purchases_by_user(Purchase.user_email_to_id(current_user.email))
@@ -40,9 +41,38 @@ class AddInventoryForm(FlaskForm):
     quantity = StringField('Quantity', validators=[DataRequired()])
     submit = SubmitField('Register Item')
 
+class EditQuantityForm(FlaskForm):
+    name = StringField('Item Name', validators=[DataRequired()])
+    category = StringField('Category', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    price = StringField('Price', validators=[DataRequired()])
+    imageurl = StringField('Image url', validators=[DataRequired()])
+    quantity = StringField('Quantity', validators=[DataRequired()])
+    submit = SubmitField('Register Item')
+
 @bp.route('/addinventory', methods=['GET', 'POST'])
 def addinventory():
     form = AddInventoryForm()
+    if form.validate_on_submit():
+        try:
+            ret = Product.add_product(current_user.id,
+                        form.name.data.strip(), 
+                        form.category.data.strip(),
+                        form.description.data.strip(),
+                        form.price.data.strip(),
+                        form.imageurl.data.strip(),
+                        form.quantity.data.strip()
+                        )
+            if ret is not None:
+                flash('User Information Updated')
+            return redirect(url_for('inventory.seller'))
+        except BadUpdateException as e:
+            flash(e.toString())
+    return render_template('addinventory.html', form=form)
+
+@bp.route('/quantityInventory', methods=['GET', 'POST'])
+def quantityInventory():
+    form = EditQuantityForm()
     if form.validate_on_submit():
         try:
             ret = Product.add_product(current_user.id,
--- app/models/seller.py
+++ app/models/seller.py
@@ -19,25 +19,6 @@ WHERE Users.user_id = :user_id AND Users.user_id = Products.user_id
 ''',
                               user_id=user_id)
         return [Inventory(*row) for row in rows]
-
-    @staticmethod
-    def get_all_inventories_by_product(product_id):
-        rows = app.db.execute('''
-SELECT Users.user_id, Products.product_id, Products.name, Products.price, Products.quantity
-FROM Users, Products
-WHERE Products.product_id = :product_id AND Users.user_id = Products.user_id
-''',
-                              product_id=product_id)
-        return [Inventory(*row) for row in rows]
-    
-    @staticmethod
-    def add_product(product_id, user_id, category, name, description, price, imageurl, quantity, available, avg_rating):
-        rows = app.db.execute('''
-INSERT INTO Products
-VALUES (:product_id, :user_id, :category, :name, :description, :price, :imageurl, :quantity, :available, :avg_rating)
-''',
-                              user_id =user_id, product_id = product_id)
-        return [Inventory(*row) for row in rows]
     
     @staticmethod
     def remove_product(user_id, product_id):
@@ -46,17 +27,36 @@ DELETE FROM Products
 WHERE user_id = :user_id AND product_id = :product_id
 ''',
                               user_id =user_id, product_id = product_id)
-        return [Inventory(*row) for row in rows]
+        return redirect(url_for('inventory.seller'))
+
+#     @staticmethod
+#     def get_all_inventories_by_product(product_id):
+#         rows = app.db.execute('''
+# SELECT Users.user_id, Products.product_id, Products.name, Products.price, Products.quantity
+# FROM Users, Products
+# WHERE Products.product_id = :product_id AND Users.user_id = Products.user_id
+# ''',
+#                               product_id=product_id)
+#         return [Inventory(*row) for row in rows]
     
-    @staticmethod
-    def update_product(user_id, product_id, quantity):
-        rows = app.db.execute('''
-UPDATE Products
-SET quantity = :quantity
-WHERE user_id = :user_id AND product_id = :product_id
-''',
-                              user_id =user_id, product_id = product_id, quantity= quantity)
-        return [Inventory(*row) for row in rows]
+#     @staticmethod
+#     def add_product(product_id, user_id, category, name, description, price, imageurl, quantity, available, avg_rating):
+#         rows = app.db.execute('''
+# INSERT INTO Products
+# VALUES (:product_id, :user_id, :category, :name, :description, :price, :imageurl, :quantity, :available, :avg_rating)
+# ''',
+#                               user_id =user_id, product_id = product_id)
+#         return [Inventory(*row) for row in rows]
+
+#     @staticmethod
+#     def update_product(user_id, product_id, quantity):
+#         rows = app.db.execute('''
+# UPDATE Products
+# SET quantity = :quantity
+# WHERE user_id = :user_id AND product_id = :product_id
+# ''',
+#                               user_id =user_id, product_id = product_id, quantity= quantity)
+#         return [Inventory(*row) for row in rows]
 
 
 class Fulfillment:
@@ -84,23 +84,7 @@ ORDER BY Orders.time_stamp DESC
 ''',
                               user_id=user_id)
         return [Fulfillment(*row) for row in rows]
-
-    @staticmethod
-    def get_all_fulfillment_by_order(order_id):
-        rows = app.db.execute('''
-SELECT Users.user_id, Products.name, Orders.order_id, Orders.time_stamp, Orders.total_items, Users.address, Purchases.fulfillment_status
-FROM Orders, Products, Purchases, Users
-WHERE (
-    Orders.order_id = Purchases.order_id AND
-    Orders.user_id = Users.user_id AND
-    Products.product_id = Purchases.pid AND
-    Orders.order_id = :order_id
-)
-ORDER BY Orders.time_stamp DESC
-''',
-                              order_id=order_id)
-        return [Fulfillment(*row) for row in rows]
-
+    
     @staticmethod
     def mark_fulfilled(order_id, product_id, fulfillment_status):
         rows = app.db.execute('''
@@ -110,3 +94,21 @@ WHERE order_id = :order_id AND product_id = :product_id
 ''',
                               order_id = order_id, fulfillment_status = fulfillment_status)
         return [Fulfillment(*row) for row in rows]
+
+#     @staticmethod
+#     def get_all_fulfillment_by_order(order_id):
+#         rows = app.db.execute('''
+# SELECT Users.user_id, Products.name, Orders.order_id, Orders.time_stamp, Orders.total_items, Users.address, Purchases.fulfillment_status
+# FROM Orders, Products, Purchases, Users
+# WHERE (
+#     Orders.order_id = Purchases.order_id AND
+#     Orders.user_id = Users.user_id AND
+#     Products.product_id = Purchases.pid AND
+#     Orders.order_id = :order_id
+# )
+# ORDER BY Orders.time_stamp DESC
+# ''',
+#                               order_id=order_id)
+#         return [Fulfillment(*row) for row in rows]
+
+
