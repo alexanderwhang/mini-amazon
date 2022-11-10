4ec48fb050c58c78b38bcf9b2360a7929cb27653
Aaric Han <ah496@duke.edu>
2022-11-10 00:36:53 -0500

    Test commit

--- app/index.py
+++ app/index.py
@@ -6,6 +6,7 @@ from .models.product import Product
 from .models.order import Order
 from .models.purchase import Purchase
 from .models.user import User
+from .models.review import Review
 
 from flask import Blueprint
 bp = Blueprint('index', __name__)
@@ -18,11 +19,14 @@ def index():
     # find the products current user has bought:
     if current_user.is_authenticated:
         orders = Purchase.get_all_purchases_by_user(Purchase.user_email_to_id(current_user.email))
+        reviews = Review.get_all_by_uid(Review.user_email_to_id(current_user.email))
         # print(current_user.email)
     else:
         orders = None
+        reviews = None
     # render the page by adding information to the index.html file
     return render_template('index.html',
                            avail_products=products,
                            purchase_history=orders,
-                           all_products = products)
+                           all_products = products,
+                           all_user_reviews = reviews)
--- db/sqlLoad/loadProductReviewTable.sql
+++ db/sqlLoad/loadProductReviewTable.sql
@@ -1,1 +1,4 @@
-\COPY ProductReviews FROM 'ProductReviews.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Review FROM 'Review.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.review_id_seq',
+                         (SELECT MAX(id)+1 FROM Review),
+                         false);
