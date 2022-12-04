77cdd4ddb2f0b647ccb0483eb10c1194d7db0062
Alex Whang <amw166@duke.edu>
2022-12-03 23:55:19 -0500

    documentation added

--- app/save.py
+++ app/save.py
@@ -8,6 +8,9 @@ from flask import Blueprint
 from flask_sqlalchemy import SQLAlchemy
 bp = Blueprint('save', __name__)
 
+# SAVED FOR LATER PAGE:
+
+# the Save class has all the components to load the saved for later feature on our ecommerce site
 class Save:
     def __init__(self, uid, pid, name, price, time):
         self.uid = uid
@@ -16,6 +19,7 @@ class Save:
         self.price = price
         self.time = time
     
+    # this method gets the items saved for later for a specific user
     @staticmethod
     def get_by_uid(uid):
         rows = app.db.execute('''
@@ -24,30 +28,39 @@ FROM Saved, Products
 WHERE uid = :uid
 AND Saved.pid = Products.id
 ORDER BY time DESC
-''',
-                              uid=uid)
+''', uid=uid)
         return [Save(*row) for row in rows]
 
+# save loads all the relevant information from the database
 @bp.route('/save', methods=['GET', 'POST'])
 @bp.route('/save/<action>/<uid>/<pid>', methods=['GET', 'POST'])
 def save(action=None, uid=None, pid=None):
-    user = User.get(current_user.id)
-    saved = Save.get_by_uid(current_user.id)
+    user = User.get(current_user.id) # gets the user information
+    saved = Save.get_by_uid(current_user.id) # gets the items saved for later
 
+    # the posting methods allow for customizability of the saved for later feature
+    # including adding items and deleting items
     if request.method == "POST":
+        # allows users to add items back to cart
         if action == 'add':
+            # delete the item from saved for later table
             app.db.execute('''DELETE FROM Saved
-            WHERE uid = :uid AND pid = :pid''', uid=uid, pid=pid)
+            WHERE uid = :uid AND pid = :pid
+            ''', uid=uid, pid=pid)
 
+            # add item to cart
             app.db.execute('''INSERT INTO Carts (uid, pid, quantity, time_added_to_cart)
             VALUES (:uid, :pid, 1, current_timestamp(0))
-            RETURNING id''', uid=uid, pid=pid)
-
+            RETURNING id
+            ''', uid=uid, pid=pid)
             return redirect(url_for('save.save'))
         
+        # allows users to delete items from saved for later list
         if action == 'delete':
             app.db.execute('''DELETE FROM Saved
-            WHERE uid = :uid AND pid = :pid''', uid=uid, pid=pid)
+            WHERE uid = :uid AND pid = :pid
+            ''', uid=uid, pid=pid)
             return redirect(url_for('save.save'))
 
+    # load saved html template
     return render_template('saved.html', title='Saved', user=user, saved=saved)
